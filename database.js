7998e615e40176c241220b7cd93877a54fae1160
rickyalatorre <alatorre1160@yahoo.com>
2023-08-29 13:01:42 -0700

    changes to database.js

--- database.js
+++ database.js
@@ -2,7 +2,8 @@
 
 const pgp = require('pg-promise')();
 
-const db = pgp(process.env.LOCAL_POSTGRES || process.env.DATABASE_URL);
- // && process.env.DATABASE_URL
+
+const db = pgp(process.env.DATABASE_URL);
+ 
 
 module.exports = db;
