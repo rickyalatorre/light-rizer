fb741c3f511cd281f10efdf59f12298d13b9703c
rickyalatorre <alatorre1160@yahoo.com>
2023-08-30 10:01:05 -0700

    add cn object so variable db can use inside pgp function

--- database.js
+++ database.js
@@ -1,9 +1,14 @@
-
-
 const pgp = require('pg-promise')();
 
+// const db = pgp(process.env.DATABASE_URL);
 
-const db = pgp(process.env.DATABASE_URL);
- 
+const cn = {
+  connectionString: process.env.DATABASE_URL,
+  max: 20,
+  ssl: {
+    rejectUnauthorized: false
+  }
+};
+var db = pgp(cn);
 
 module.exports = db;
