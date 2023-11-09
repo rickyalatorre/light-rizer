ffc931e5440161076c600cdb940d50df218cfb46
rickyalatorre <alatorre1160@yahoo.com>
2023-11-08 18:00:30 -0800

    saved after using location for timer

--- app.js
+++ app.js
@@ -372,17 +372,6 @@ if(new Date().toDateString() == dataObj.in2Days){
 // calling clear timer in /clear-phone route
 let timerInterval;
 function timeInterval(dataObj){
-  // const now = new Date();
-  // let hours = now.getHours();
-  // const minutes = String(now.getMinutes()).padStart(2, '0');
-  // const seconds = String(now.getSeconds()).padStart(2, '0');
-  // const ampm = hours >= 12 ? 'PM' : 'AM';
-  //
-  // hours = hours % 12 || 12; // Convert to 12-hour format
-  //
-  // const currentTime = `${hours}:${minutes}:${seconds} ${ampm}`;
-  // console.log(`timer started at ${currentTime} on ${dataObj.dayOfSubmit} ....`);
-
   let date = new Date();
 console.log('dataObj:',dataObj);
   let timerStart=date.toLocaleTimeString('en-US',{timeZone:dataObj.location});
